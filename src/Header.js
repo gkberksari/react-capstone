5e273c3c21cdbb5d0f9242665e18a40f59477274
gokberk <gokberksarihr@gmail.com>
2025-02-06 22:30:29 +0300

    Basic components and semantic HMTL tags added

---
+++ src/Header.js
@@ -0,0 +1,8 @@
+function Header(){
+    return(
+        <header>
+            <img src="logo.svg"/>
+            Little Lemon
+        </header>
+    );
+}
---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+function Nav(){
+    return(
+        <ul>
+            <li>
+                <a>Menu</a>
+            </li>
+            <li>
+                <a>Contact</a>
+            </li>
+        </ul>
+    );
+}
